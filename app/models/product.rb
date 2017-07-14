82f812d8b0d613223517f01fddd0fe9eba37e616
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-07-14 14:40:56 -0700

    add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :order_items
+  validates :name, presence: { message: "It must have a name!" }
+  validates :description, presence: { message: "It must have a description!" }
+  validates :price, presence: { message: "It must have a price you animal!" } 
+
 end
